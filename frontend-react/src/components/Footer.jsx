886bdb7281c6fdab70fd7c1627d9bc04abc2de91
Jesus Torres <torres.jesus2143@gmail.com>
2023-11-26 16:13:38 -0600

    fixed serval bugs and included new css features

--- frontend-react/src/components/Footer.jsx
+++ frontend-react/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
 	return (
-		<footer className=" text-light footer">
+		<footer className="footer">
 			<div className="f-div">
 				{/* <div className="row"> */}
 				<p className="pt-2">Coding Since - Jan 2021</p>
